a4acd608df00ea62c52d5f5ae8716dc8f5c36412
gongzijing <1105599360@qq.com>
2025-11-10 23:14:58 +0800

    feat: prototype 和 __proto__ 的学习

--- prototype_proto.js
+++ prototype_proto.js
@@ -1,1 +1,26 @@
-// 初始化
+// prototype  只存在于构造函数上   __proto__ 存在每个对象上
+// prototype  和 proto
+
+// 重写Object
+// function Object(name) {
+//     this.name = name 
+// }
+
+// const obj = new Object('实例')
+
+function Person(id){
+    this.id = id
+}
+Person.prototype.sayName = function(){
+    console.log(this.id);
+}
+const me = new Person('1')
+
+console.log(Person.prototype);
+me.sayName()
+console.log(Object);
+//console.log(me.id);
+console.log(Person.prototype.__proto__ === Object.prototype);
+
+
+
